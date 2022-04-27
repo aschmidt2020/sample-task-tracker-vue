3d3546f62cf890bbc161802a8c542efc466ba261
Audrey Schmidt <audsch@outlook.com>
2022-04-27 07:04:41 -0500

    feat: added header component and button component - no functionality:

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button :style="{ background: color }" class="btn" @click="onClick()">{{text}}</button>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line
+    name: 'Button',
+    props: {
+        text: String,
+        color: String
+    },
+    methods: {
+        onClick() {
+            console.log('Click')
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
